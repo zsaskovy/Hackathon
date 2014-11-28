aada20641ac258184898791e6e7329d183fdacca
Zsolt Saskovy <v-zsaskovy@expedia.com>
2014-11-28 19:30:45 +0100

    a

--- Strategy.lua
+++ Strategy.lua
@@ -1,9 +1,9 @@
-function strategy(bot) 
-	local l = MapTools:getMyLocation(bot)
+function strategy(marine) 
+	local l = MapTools:getMyLocation(marine)
 	
 	if (getNearEnemies(l) ~= nil) then
 		local enemies = getNearEnemies(l)
-		if (canAttack()) then
+		if (canAttack(marine)) then
 			attack(enemies)
 		else
 			escape(enemies)
@@ -11,10 +11,10 @@ function strategy(bot)
 	elseif (getNearItems(l) ~= nil) then
 		local nearItems = getNearItems(l)
 		
-		if (lowHealth() and hasHealing(nearItems)) then
+		if (lowHealth(marine) and hasHealing(nearItems)) then
 			item = getHealing(nearItems)
 			moveTo(nearItems.location)
-		elseif (lowAmmo() and hasAmmo(nearItems)) then
+		elseif (lowAmmo(marine) and hasAmmo(nearItems)) then
 			item = getAmmo(nearItems)
 			moveTo(nearItems.location)
 		else 
@@ -32,7 +32,8 @@ end
 function getNearItems(nearItems)
 end
 
-function canAttack()
+function canAttack(marine)
+	return (marine.AttackPoints > 0 and not lowAmmo(marine))
 end
 
 function attack(enemies)
@@ -47,14 +48,12 @@ end
 function hasHealing(nearItems)
 end
 
-function lowAmmo()
+function lowAmmo(marine)
+	return false
 end
 
 function hasAmmo(nearItems)
 end
 
 function moveAlong(location)
-end
-
-local function getMyLocation()
 end
