45669bc2719bb37e28e6b099d7ae43a610ed217a
Zsolt Saskovy <v-zsaskovy@expedia.com>
2014-11-28 19:23:30 +0100

    a

--- Marvin.lua
+++ Marvin.lua
@@ -1,5 +1,7 @@
 Marvin = class( "Marvin", DeathMatchMarine )
 
+local dx, dy
+
 function Marvin:select_mode()
 	return "advance"
 end
@@ -7,13 +9,11 @@ end
 function Marvin:provide_steps(prev)
 	local marine,err = Game.Map:get_entity("marine-1")
 
-  return { 
-    { 
-      Command = "move", 
-      Path = { 
-          { X = marine.Bounds.X - 1, Y = 1 } 
-        } 
-    },
-    { Command = "done" } 
-  }
+	local command = Strategy:strategy(marine)
+
+	return { 
+		command,
+		{ Command = "done" } 
+	}
 end
+
