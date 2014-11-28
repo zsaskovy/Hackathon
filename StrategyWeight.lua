af61523734db1f59578d7fedcea5305038e0fbff
Gergo Peter Nagy <Gergo_Peter_Nagy@epam.com>
2014-11-28 21:08:52 +0100

    weights

--- StrategyWeight.lua
+++
@@ -1,3 +0,0 @@
-function weights()
-    
-end
---
+++ StrategyWeights.lua
@@ -0,0 +1,8 @@
+function strategyWeights()
+    local weights = {
+        Explorer =  { Health=0.0 ,Armor=0.0, CanDodge=0.0, Invisible=0.0, Wounds=0.0, AttackPoints=0.0, Accuracy=0.0, Deadly=0.0, MaxRange=0.0, IgnoresSight=0.0, IgnoresFailedDices=0.0, CanDoAimed=0.0, AttackCount=0.0, MovePoints=0.0, IgnoresCollisions=0.0, MoveCount=0.0 }
+        Camper =    { Health=0.0, Armor=0.0, CanDodge=0.0, Invisible=0.0, Wounds=0.0, AttackPoints=0.0, Accuracy=0.0, Deadly=0.0, MaxRange=0.0, IgnoresSight=0.0, IgnoresFailedDices=0.0, CanDoAimed=0.0, AttackCount=0.0, MovePoints=0.0, IgnoresCollisions=0.0, MoveCount=0.0 }
+        Aggressive ={ Health=0.0, Armor=0.0, CanDodge=0.0, Invisible=0.0, Wounds=0.0, AttackPoints=0.0, Accuracy=0.0, Deadly=0.0, MaxRange=0.0, IgnoresSight=0.0, IgnoresFailedDices=0.0, CanDoAimed=0.0, AttackCount=0.0, MovePoints=0.0, IgnoresCollisions=0.0, MoveCount=0.0 }
+    }
+    return weights
+end
