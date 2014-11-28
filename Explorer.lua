d5e5ddb23db981cd3b72fc233dd35996e2c80c8f
unknown <Ronald_Legmann@EPHUBUDW0685.budapest.epam.com>
2014-11-28 23:21:02 +0100

    no message

--- Explorer.lua
+++ Explorer.lua
@@ -7,10 +7,13 @@ function Explorer:nextMove(marine)
 	print_r(possibleItems)
 	math.randomseed(12323131231212312)
 	local nextPosition = nil
+	local headToPickup = false
+	
 	if (#possibleItems > 0) then
 		local r = math.random(1, #possibleItems)
 		nextPosition = possibleItems[r][1]
 		nextPosition = coord(nextPosition.Bounds.X, nextPosition.Bounds.Y)
+		headToPickup = true
 	elseif(#possibleCells > 0) then
 		local r = math.random(1, #possibleCells)
 		nextPosition = possibleCells[r]
@@ -30,43 +33,3 @@ function Explorer:nextMove(marine)
 	}
 
 end
-
---[[Explorer = class( "Explorer", DeathMatchMarine )
-
-function Explorer:select_mode()
-	return "advance"
-end
-
-function Explorer:provide_steps(prev)
-	local marine,err = Game.Map:get_entity("marine-1")
-	
-	local direction = MapTools.top
-	local newDirection = MapTools.top
-	
-	--where to move
-	function tryMove()
-		local nextCoord = MapTools:addCoordinate(marine.Bounds, MapTools:turnDirection(direction, newDirection))
-		local tmpCoord = MapTools:turnDirection(direction, newDirection)
-		print("nextcoord " .. tmpCoord.X .. tmpCoord.Y)
-		return MapTools:isPassable(nextCoord)
-	end
-	
-	while not tryMove() do
-		newDirection = MapTools:nextDirection(direction)
-		print ("attempt direction" .. direction.X .. direction.Y)
-		
-	end
-	
-	newOffset = {X = marine.Bounds.X + newDirection.X, Y = marine.Bounds.Y + newDirection.Y}
-	
-  return { 
-    { 
-      Command = "move", 
-      Path = { 
-          newOffset
-        } 
-    },
-    { Command = "done" } 
-  }
-end
-]]--
