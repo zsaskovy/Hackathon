cc8e150a45811022b25f4f1a79d2e159ef80bbcf
Zsolt Saskovy <v-zsaskovy@expedia.com>
2014-11-28 20:56:44 +0100

    a

--- Marvin.lua
+++ Marvin.lua
@@ -1,5 +1,7 @@
 Marvin = class( "Marvin", DeathMatchMarine )
 
+local baseStrategy = "Explorer"
+
 function Marvin:select_mode()
 	return "advance"
 end
@@ -7,12 +9,21 @@ end
 function Marvin:provide_steps(prev)
 	local marine,err = Game.Map:get_entity("marine-1")
 
-	--local command = Strategy:strategy(marine)
-	local c = MapTools:getMyLocation(marine)
-	getNearItems(marine)
+	local strategy = determineStrategy(marine, baseStrategy)
+	local command = {}
+	
+	if (strategy == "Explorer") then
+		command = Explorer:nextMove(marine)
+		
+	elseif (strategy == "Aggressive") then
+		command = Aggressive:nextMove(marine)
+		
+	elseif (strategy == "Camper") then
+		command = Camper:nextMove(marine)
+	end
 
 	return { 
---		command,
+		command,
 		{ Command = "done" } 
 	}
 end
