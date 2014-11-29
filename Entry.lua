8f7205f7b05670e63f52984157e4e07ca214180d
Gergo Peter Nagy <Gergo_Peter_Nagy@epam.com>
2014-11-29 05:41:22 +0100

    Marvins list created

--- Entry.lua
+++ Entry.lua
@@ -1,3 +1,8 @@
+Marvins = {}
+
 function CreateMarine(player_index, marine_id, instance_index)
+
+    Marvins[#Marvins + 1] = marine_id
+
 	return Marvin:new(player_index, marine_id, instance_index)
 end
